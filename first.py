b16bb45198dbad59cbbf2edf77922e1f11554ad8
Cipher_Professor <mohsinmanzoor1913@gmail.com>
2022-08-13 12:58:03 +0530

    modified:   first.py

---
+++ first.py
@@ -0,0 +1,20 @@
+print ("Hello world")
+print ("My name is Mohsin Manzoor Bhat")
+
+name ="Mohsin"
+age = 24
+
+print(name)
+print(age)
+
+age2 =24.5
+print(age2)
+is_adult =True
+is_not_adult = False
+
+first_name ="Mohsin Manzoor"
+last_name = "Bhat"
+age3 =24
+Engineer = True
+
+print(first_name,last_name ,"is my name and i am " ,age3, "years old",)
