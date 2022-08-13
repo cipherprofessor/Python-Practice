0f0ebf75d0ff0f5ae77b2a5942c312fbe5a8eeec
Cipher_Professor <mohsinmanzoor1913@gmail.com>
2022-08-13 13:12:50 +0530

    new file:   input_output.py 	new file:   tempCodeRunnerFile.py

---
+++ input_output.py
@@ -0,0 +1,19 @@
+# from html.entities import name2codepoint
+
+
+# #Input
+
+# name1= input("What is your name ? ")
+# print("Hello my dear " + name1)
+# name2 = input("What is your father's name ")
+# print("Ohh "+name2+" is your father , That's great")
+
+# #Type Conversation
+
+# old_age = input("What is your old age ")
+# new_age = int(old_age)+20
+# print("You are now 20 years younger and your age is" , new_age)
+
+float1 = input("Enter any floating number you want ? ")
+
+print("You entered ",float(float1))
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,2 @@
+dob = input("What is your D.O.B ? ")
+print("you were born on",dob)
