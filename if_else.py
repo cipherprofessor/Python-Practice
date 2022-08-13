0689cc65505eebc41af9153b7d89920d31599875
Cipher_Professor <mohsinmanzoor1913@gmail.com>
2022-08-13 14:03:50 +0530

    new file:   if_else.py 	new file:   strings.py

---
+++ if_else.py
@@ -0,0 +1,10 @@
+age =0
+if age >=18:
+    print("You Can Drive")
+    print("Enjoy Driving")
+elif age<18 and age >3:
+    print("You are under age and you cannot drive")
+else:
+    print("You are child")
+print("Thank You For Response")
+
---
+++ strings.py
@@ -0,0 +1,7 @@
+name = "Mohsin Manzoor Bhat"
+print(name.upper())
+print(name.find('o'))
+print('m' in name)
+
+# //return only int value , removes decimal like 5/2 =2.5 and 5//2 =2
+# ** = to the power
