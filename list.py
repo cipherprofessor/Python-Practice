1dcb5c4f1f2565100db03794bb37f081650424bc
Cipher_Professor <mohsinmanzoor1913@gmail.com>
2022-08-15 23:19:09 +0530

    new file:   list.py

---
+++ list.py
@@ -0,0 +1,22 @@
+marks =[20,40,60,80,100]
+print(marks)
+
+#we can use print(marks-[1]) --> it will start from last 
+
+#appends adds to last of list
+marks.append(120) 
+print(marks)
+print(len(marks))
+
+#clear empeties the list
+marks.clear()
+print(marks)
+
+#adds 99 marks at 0th position
+marks.insert(0,99)
+print(marks)
+
+#prints length of marks /list
+print(len(marks))
+
+
